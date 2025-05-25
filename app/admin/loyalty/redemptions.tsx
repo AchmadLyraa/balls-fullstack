aef037e9e70e58988fedc27c414fc8fbc4e0111b
NathanaelGT <gordontanu2004@gmail.com>
2025-05-25 22:28:39 +0700

    fix redemption modal

--- app/admin/loyalty/redemptions.tsx
+++ app/admin/loyalty/redemptions.tsx
@@ -58,6 +58,7 @@ export default function Redemptions({ redemptions }: RedemptionsProps) {
     } finally {
       setIsOpen(false);
       setNewStatus("");
+      setIsLoading(false);
     }
   };
 
